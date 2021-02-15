edd8cb4fdc583cc20531d78c6ab0c34bc497bfab
AcedYu <alexyu299@hotmail.com>
2021-02-15 15:04:58 -0800

    Minimum Requirements Complete

--- app.js
+++ app.js
@@ -1,46 +1,66 @@
-//javascript functions
-//entry feed creation function
-// var feed = (username) => {
-//   var $app = $('#app');
-//   $app.html();
-//   var data = removeDupes(streams.home);
-//   var index = data.length - 1;
-//   var counter = 1;
-//   while(index >= 0) {
-//     var tweet = data[index];
-//     var $tweet = $('<div class="tweet"></div>');
-//     $tweet.text(`${counter}@ ${tweet.user}: ${tweet.message}`);
-//     $tweet.appendTo($app);
-//     index -= 1;
-//     counter++;
-//   }
-// }
 var feed = () => {
   var $app = $('#app');
-  $app.html('');
+  $app.html(' ');
   var index = streams.home.length - 1;
   while(index >= 0) {
     var tweet = streams.home[index];
     var time = jQuery.timeago(tweet.created_at);
     var $tweet = $(
       `<div class="tweet">
-      <img src = "${tweet.profilePhotoURL}" class = "profile-photo"/>
-      <p class = "username" id = "${tweet.user}">@${tweet.user}</p>
-      <p class = "message">${tweet.message}</p>
-      <p class = "timestamp">${time}</p>
-      <img src = "./assets/icons/comment.png" class = "icon comment" />
-      <img src = "./assets/icons/retweet.png" class = "icon retweet" />
-      <img src = "./assets/icons/like.png" class = "icon like" />
-      <img src = "./assets/icons/share.png" class = "icon share" />
+        <table align = "center">
+          <thead>
+            <tr>
+              <th><img src = "${tweet.profilePhotoURL}" class = "profile-photo"/></th>
+              <th class = "username" id = "${tweet.user}">@${tweet.user}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class = "message" colspan = "2">${tweet.message}</td>
+            </tr>
+            <tr>
+              <td class = "timestamp" colspan = "2">${time}</td>
+            </tr>
+            <tr>
+              <td colspan = "2" id = "buttonlayout">
+                <i class="far fa-comment icon comment" />
+                <i class="fas fa-retweet icon retweet" />
+                <i class="far fa-thumbs-up icon like" />
+                <i class="far fa-share-square icon share" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <br/>
       </div>`);
     $tweet.appendTo($app);
     index -= 1;
   }
   $('.username').click(userfeed);
+  $("i").hover(function() {
+    $(this).css("color", "lightskyblue");
+  }, function() {
+    $(this).css("color", "black");
+  });
+
+  $(".username").hover(function() {
+    $(this).css("color", "lightskyblue");
+  }, function() {
+    $(this).css("color", "black");
+  });
 }
 var userfeed = (event) => {
   var $app = $('#app');
-  $app.html('');
+  $app.html(' ');
+  var $backbutton = $(
+    `<div class="container">
+      <div class="center">
+        <button id="back">Back To Home</button>
+      </div>
+    </div>`
+  );
+  $backbutton.appendTo($app);
+  $('#back').click(feed);
   var name = event.target.id;
   var index = streams.home.length - 1;
   while(index >= 0) {
@@ -49,14 +69,31 @@ var userfeed = (event) => {
       var time = jQuery.timeago(tweet.created_at);
       var $tweet = $(
         `<div class="tweet">
-        <img src = "${tweet.profilePhotoURL}" class = "profile-photo"/>
-        <p class = "username" id = "${tweet.user}">@${tweet.user}</p>
-        <p class = "message">${tweet.message}</p>
-        <p class = "timestamp">${time}</p>
-        <img src = "./assets/icons/comment.png" class = "icon comment" />
-        <img src = "./assets/icons/retweet.png" class = "icon retweet" />
-        <img src = "./assets/icons/like.png" class = "icon like" />
-        <img src = "./assets/icons/share.png" class = "icon share" />
+          <table align = "center">
+            <thead>
+              <tr>
+                <th><img src = "${tweet.profilePhotoURL}" class = "profile-photo"/></th>
+                <th class = "username" id = "${tweet.user}">@${tweet.user}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class = "message" colspan = "2">${tweet.message}</td>
+              </tr>
+              <tr>
+                <td class = "timestamp" colspan = "2">${time}</td>
+              </tr>
+              <tr>
+                <td colspan = "2" id = "buttonlayout">
+                  <i class="far fa-comment icon comment" />
+                  <i class="fas fa-retweet icon retweet" />
+                  <i class="far fa-thumbs-up icon like" />
+                  <i class="far fa-share-square icon share" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <br/>
         </div>`);
     $tweet.appendTo($app);
     index -= 1;
@@ -64,6 +101,17 @@ var userfeed = (event) => {
       index -= 1;
     }
   }
+  $("i").hover(function() {
+    $(this).css("color", "lightskyblue");
+  }, function() {
+    $(this).css("color", "black");
+  });
+
+  $(".username").hover(function() {
+    $(this).css("color", "lightskyblue");
+  }, function() {
+    $(this).css("color", "black");
+  });
 }
 
 // jquery activation on the document
@@ -71,3 +119,15 @@ $(document).ready(() => {
   feed(event);
   $('#update-feed').click(feed);
 });
+
+  // var $tweet = $(
+  //   `<div class="tweet">
+  //   <img src = "${tweet.profilePhotoURL}" class = "profile-photo"/>
+  //   <p class = "username" id = "${tweet.user}">@${tweet.user}</p>
+  //   <p class = "message">${tweet.message}</p>
+  //   <p class = "timestamp">${time}</p>
+  //   <img src = "./assets/icons/comment.png" class = "icon comment" />
+  //   <img src = "./assets/icons/retweet.png" class = "icon retweet" />
+  //   <img src = "./assets/icons/like.png" class = "icon like" />
+  //   <img src = "./assets/icons/share.png" class = "icon share" />
+  //   </div>`);
