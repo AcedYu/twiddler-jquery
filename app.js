4f6a5b32b06c612a248f5ba35865af863ffd38d3
AcedYu <alexyu299@hotmail.com>
2021-02-19 15:43:39 -0800

    Added Friends List

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
+var headerSet = () => {
+  var $head = $('header');
+  var $header = $(
+    `<header>
+      <h1>Twiddler</h1>
+      <div id = "the-button" class="container">
+        <div class="center">
+          <button id="update-feed">Update Feed</button>
+        </div>
+      </div>
+    </header>`
+  );
+  $header.appendTo($head);
+}
+
+var friendset = () => {
+  var $frienddiv = $('#friendlist');
+  var $component = $(
+    `<h2>Friends List</h2>
+      <ul>
+      <li class = 'listname' id = 'douglascalhoun'><img src = "./assets/img/douglascalhoun.png" class = "profile-photo"/>douglascalhoun</li>
+      <li class = 'listname' id = 'mracus'><img src = "./assets/img/mracus.png" class = "profile-photo"/>mracus</li>
+      <li class = 'listname' id = 'sharksforcheap'><img src = "./assets/img/sharksforcheap.png" class = "profile-photo"/>sharksforcheap</li>
+      <li class = 'listname' id = 'shawndrost'><img src = "./assets/img/shawndrost.png" class = "profile-photo"/>shawndrost</li>
+      </ul>`
+    );
+  $component.appendTo($frienddiv);
+
+  $('.listname').click(userfeed);
+  $('.listname').hover(function() {
+    $(this).css("color", "lightskyblue");
+  }, function() {
+    $(this).css("color", "black");
+  });
+}
+
+//default feed function
 var feed = () => {
+  //Scroll to top of page
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  //Locate and delete all items on the app div
   var $app = $('#app');
   $app.html(' ');
+
+  //Create the feed
   var index = streams.home.length - 1;
   while(index >= 0) {
     var tweet = streams.home[index];
@@ -36,35 +78,43 @@ var feed = () => {
     $tweet.appendTo($app);
     index -= 1;
   }
+
+  //assign on click username functions
   $('.username').click(userfeed);
+
+  //assign on hover icon functions
   $("i").hover(function() {
     $(this).css("color", "lightskyblue");
   }, function() {
     $(this).css("color", "black");
   });
 
+  //username on hover functions
   $(".username").hover(function() {
     $(this).css("color", "lightskyblue");
   }, function() {
     $(this).css("color", "black");
   });
 }
+
+//userfeed function
 var userfeed = (event) => {
+  //scroll back to the top of the page
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  //locate the app div and delete all items
   var $app = $('#app');
   $app.html(' ');
-  var $backbutton = $(
-    `<div class="container">
-      <div class="center">
-        <button id="back">Back To Home</button>
-      </div>
-    </div>`
-  );
-  $backbutton.appendTo($app);
-  $('#back').click(feed);
+
+  //change button text
+  $('button').text('Back to Home')
+
+  //event target id will equal username
   var name = event.target.id;
+  //create the feed
   var index = streams.home.length - 1;
   while(index >= 0) {
     var tweet = streams.home[index];
+    //filter feed by username
     if (tweet.user === name) {
       var time = jQuery.timeago(tweet.created_at);
       var $tweet = $(
@@ -101,33 +151,31 @@ var userfeed = (event) => {
       index -= 1;
     }
   }
+
+  //assign on hover icon functions
   $("i").hover(function() {
     $(this).css("color", "lightskyblue");
   }, function() {
     $(this).css("color", "black");
   });
 
+  //username on hover functions
   $(".username").hover(function() {
     $(this).css("color", "lightskyblue");
   }, function() {
     $(this).css("color", "black");
   });
+
+  //change button text back on click
+  $('button').click(() => {
+    $('button').text('Update Feed');
+  });
 }
 
 // jquery activation on the document
 $(document).ready(() => {
+  headerSet();
+  friendset();
   feed(event);
   $('#update-feed').click(feed);
 });
-
-  // var $tweet = $(
-  //   `<div class="tweet">
-  //   <img src = "${tweet.profilePhotoURL}" class = "profile-photo"/>
-  //   <p class = "username" id = "${tweet.user}">@${tweet.user}</p>
-  //   <p class = "message">${tweet.message}</p>
-  //   <p class = "timestamp">${time}</p>
-  //   <img src = "./assets/icons/comment.png" class = "icon comment" />
-  //   <img src = "./assets/icons/retweet.png" class = "icon retweet" />
-  //   <img src = "./assets/icons/like.png" class = "icon like" />
-  //   <img src = "./assets/icons/share.png" class = "icon share" />
-  //   </div>`);
